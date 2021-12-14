68a3b72192cb0712c32cf0eabfb6b35915ed5e71
Haritha Manikonda <haritha.manikonda@gmail.com>
2021-12-13 19:31:54 -0700

    setting build number

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -57,7 +57,6 @@ export const config: WebdriverIO.Config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        build: 'test-build',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
